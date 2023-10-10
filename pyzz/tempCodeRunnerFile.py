8b3ca2c42a6729e9217848bb5b37e173c449ef74
Baghdadli abdelhadi <116256327+BaghHadi@users.noreply.github.com>
2023-10-10 18:12:21 +0100

    Add files via upload

---
+++ pyzz/tempCodeRunnerFile.py
@@ -0,0 +1,25 @@
+#ask for player 1 input
+            # if turn == 0:
+            #     col = int(input("Player 1 Make your Selection (0-6): "))
+                
+            #     if is_valid_location(board, col):
+            #         row = get_next_open_row(board, col)
+            #         drop_piece(board, row, col, 1)
+
+            #         if winning_move(board, 1):
+            #             print("Player 1 Wins!!! Congrats!!!")
+            #             game_over = True
+
+
+            # #ask for player 2 input
+            # else:
+            #     col = int(input("Player 2 Make your Selection (0-6): "))
+
+            #     if is_valid_location(board, col):
+            #         row = get_next_open_row(board, col)
+            #         drop_piece(board, row, col, 2)
+
+
+            #     if winning_move(board, 2):
+            #         print("Player 2 Wins!!! Congrats!!!")
+            #         game_over = True
